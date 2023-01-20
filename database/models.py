8b77f1f80faa5064d433e3ce12297437b05abe57
Daniel <ddeityy@gmail.com>
2023-01-21 02:11:12 +0300

    .

--- database/models.py
+++ database/models.py
@@ -5,21 +5,18 @@ db.bind(provider="sqlite", filename="db.sqlite", create_db=True)
 sql_debug(True)
 
 
-class User(db.Entity):
-    discord_id = PrimaryKey(int)
-    name = Required(str, unique=True)
-
-
 class Waifu(db.Entity):
-    waifu_id = PrimaryKey(int, auto=True)
-    anilist_id = Required(int, unique=True)
-    full_name = Required(str)
-    gender = Required(str)
-    age = Required(str, default="Unknown")
-    image_url = Required(str)
-    source_title = Required(str)
-    captured = Required(bool, default=False)
-    value = Required(int)
+    id = PrimaryKey(int, auto=True)
+    name = Required(str)
+    source = Required(str)
+    post_count = Required(int)
+    best_safe_post_id = Required(int, default=0)
+    best_safe_post_score = Required(int, default=0)
+    best_safe_post_image = Required(str, default="None")
+    best_unsafe_post_id = Required(int, default=0)
+    best_unsafe_post_score = Required(int, default=0)
+    best_unsafe_post_image = Required(str, default="None")
+    value = Required(int, default=0)
 
 
 db.generate_mapping(create_tables=True)
